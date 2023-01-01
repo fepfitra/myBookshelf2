45152f9de45a9608da9e49eebff0a6a462eb9487
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-01-01 06:19:31 -0500

    problem solved

--- src/script/component/app-bar.js
+++ src/script/component/app-bar.js
@@ -1,11 +1,13 @@
 class AppBar extends HTMLElement {
   connectedCallback(){
     this.render();
-    alert("app-bar.js")
   }
 
   render(){
     this.innerHTML = `
+      <style>
+        @import url 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css';
+      </style>
       <span class="navbar-brand mb-0 h1">myBookshelf</span>
     `;
   }
